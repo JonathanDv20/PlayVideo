15eb60f159dfb275f063c26a7296c701e0ead3b2
JonathanDv20 <casanovagloriaj156@gmail.com>
2024-09-17 19:25:13 -0600

    Add files via upload

---
+++ watch01.js
@@ -0,0 +1,10 @@
+let likeButton = document.querySelector('.like-button');
+let dislikeButton = document.querySelector('.dislike-button');
+
+likeButton.addEventListener('click', () => {
+    alert('¡Has dado like!');
+});
+
+dislikeButton.addEventListener('click', () => {
+    alert('¡Has dado dislike!');
+});
